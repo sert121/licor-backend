5b23d68fd9ae38c91bcb04c5eb3b9f2e4d3d494e
sert121 <yashrajmore29@gmail.com>
2023-04-30 23:50:34 +0530

    response content

--- main.py
+++ main.py
@@ -272,7 +272,8 @@ async def notion_code(body:Code, session: SessionContainer = Depends(verify_sess
 
     response = requests.post(url, data=json.dumps(payload), headers=headers)
     logger.info(response.json())
-    access_token = response.json()['content']['access_token']
+    response_content = response.content
+    access_token = response_content['access_token']
     logger.info("access token : ", access_token)
     
     # add_notion_docs(auth_token=body.code)
